908ebe7a4674ee41e853726dbcb60f1094a10da9
Andrew Beers <abeers@emporos.net>
2023-05-08 17:31:25 -0400

    Updates to resolve List<T> mapping as well as some hardening and updates for readability.  Notable change in IDIIEntity must be what is inherited at Optimizer Init and only those types works in packing at the top level.  Non-IDIIEntity types can still be referenced inside IDIIEntity objects as properties.

--- dii.storage.tests/OptimizerTests/ToEntityObjectTests.cs
+++ dii.storage.tests/OptimizerTests/ToEntityObjectTests.cs
@@ -15,124 +15,12 @@ namespace dii.storage.tests.OptimizerTests
         [Fact, TestPriorityOrder(100)]
         public void ToEntityObject_Prep()
         {
-            _ = Optimizer.Init(typeof(FakeEntityFour), typeof(FakeEntityThree));
+            _ = Optimizer.Init(typeof(FakeEntityThree));
 
             TestHelpers.AssertOptimizerIsInitialized();
         }
 
-        [Fact, TestPriorityOrder(101)]
-        public void ToEntityObject_Success()
-        {
-            var optimizer = Optimizer.Get();
-
-            var fakeSearchableEntity = new FakeSearchableEntity
-            {
-                SearchableStringValue = $"fakeSearchableEntity: {nameof(FakeSearchableEntity.SearchableStringValue)}",
-                CompressedStringValue = $"fakeSearchableEntity: {nameof(FakeSearchableEntity.CompressedStringValue)}",
-                ComplexSearchable = new FakeSearchableEntityTwo
-                {
-                    SearchableStringValue = $"fakeSearchableEntity: {nameof(FakeSearchableEntity.ComplexSearchable)}.{nameof(FakeSearchableEntity.ComplexSearchable.SearchableStringValue)}",
-                    CompressedStringValue = $"fakeSearchableEntity: {nameof(FakeSearchableEntity.ComplexSearchable)}.{nameof(FakeSearchableEntity.ComplexSearchable.CompressedStringValue)}"
-                }
-            };
-
-            var entity = (dynamic)optimizer.ToEntityObject<FakeSearchableEntity>(fakeSearchableEntity);
-
-            var searchableStringValue = entity.@string as string;
-            var compressedStringValue = entity.p as string;
-            var complexSearchableStringValue = entity.complex.@string as string;
-            var complexCompressedStringValue = entity.complex.p as string;
-
-            Assert.Equal(fakeSearchableEntity.SearchableStringValue, searchableStringValue);
-            Assert.Equal("kdkrZmFrZVNlYXJjaGFibGVFbnRpdHk6IENvbXByZXNzZWRTdHJpbmdWYWx1ZQ==", compressedStringValue);
-            Assert.Equal(fakeSearchableEntity.ComplexSearchable.SearchableStringValue, complexSearchableStringValue);
-            Assert.Equal("kdk9ZmFrZVNlYXJjaGFibGVFbnRpdHk6IENvbXBsZXhTZWFyY2hhYmxlLkNvbXByZXNzZWRTdHJpbmdWYWx1ZQ==", complexCompressedStringValue);
-        }
-
-        [Fact, TestPriorityOrder(102)]
-        public void ToEntityObject_SuccessMultipleSameSubEntity()
-        {
-            var optimizer = Optimizer.Get();
-
-            var fakeSearchableEntityFour = new FakeSearchableEntityFour
-            {
-                SearchableStringValue = $"fakeSearchableEntityFour: {nameof(FakeSearchableEntityFour.SearchableStringValue)}",
-                CompressedStringValue = $"fakeSearchableEntityFour: {nameof(FakeSearchableEntityFour.CompressedStringValue)}",
-                ComplexSearchable1 = new FakeSearchableEntityTwo
-                {
-                    SearchableStringValue = $"fakeSearchableEntityFour: {nameof(FakeSearchableEntityFour.ComplexSearchable1)}.{nameof(FakeSearchableEntityFour.ComplexSearchable1.SearchableStringValue)}",
-                    CompressedStringValue = $"fakeSearchableEntityFour: {nameof(FakeSearchableEntityFour.ComplexSearchable1)}.{nameof(FakeSearchableEntityFour.ComplexSearchable1.CompressedStringValue)}"
-                },
-                ComplexSearchable2 = new FakeSearchableEntityTwo
-                {
-                    SearchableStringValue = $"fakeSearchableEntityFour: {nameof(FakeSearchableEntityFour.ComplexSearchable2)}.{nameof(FakeSearchableEntityFour.ComplexSearchable2.SearchableStringValue)}",
-                    CompressedStringValue = $"fakeSearchableEntityFour: {nameof(FakeSearchableEntityFour.ComplexSearchable2)}.{nameof(FakeSearchableEntityFour.ComplexSearchable2.CompressedStringValue)}"
-                }
-            };
-
-            var entity = (dynamic)optimizer.ToEntityObject<FakeSearchableEntityFour>(fakeSearchableEntityFour);
-
-            Assert.NotNull(entity);
-
-            var searchableStringValue = entity.@string as string;
-            var compressedStringValue = entity.p as string;
-
-            var complex1SearchableStringValue = entity.complex1.@string as string;
-            var complex1CompressedStringValue = entity.complex1.p as string;
-            var complex2SearchableStringValue = entity.complex2.@string as string;
-            var complex2CompressedStringValue = entity.complex2.p as string;
-
-            Assert.Equal(fakeSearchableEntityFour.SearchableStringValue, searchableStringValue);
-            Assert.Equal("kdkvZmFrZVNlYXJjaGFibGVFbnRpdHlGb3VyOiBDb21wcmVzc2VkU3RyaW5nVmFsdWU=", compressedStringValue);
-            Assert.Equal(fakeSearchableEntityFour.ComplexSearchable1.SearchableStringValue, complex1SearchableStringValue);
-            Assert.Equal("kdlCZmFrZVNlYXJjaGFibGVFbnRpdHlGb3VyOiBDb21wbGV4U2VhcmNoYWJsZTEuQ29tcHJlc3NlZFN0cmluZ1ZhbHVl", complex1CompressedStringValue);
-            Assert.Equal(fakeSearchableEntityFour.ComplexSearchable2.SearchableStringValue, complex2SearchableStringValue);
-            Assert.Equal("kdlCZmFrZVNlYXJjaGFibGVFbnRpdHlGb3VyOiBDb21wbGV4U2VhcmNoYWJsZTIuQ29tcHJlc3NlZFN0cmluZ1ZhbHVl", complex2CompressedStringValue);
-        }
-
-        [Fact, TestPriorityOrder(103)]
-        public void ToEntityObject_SuccessRecursive()
-        {
-            var optimizer = Optimizer.Get();
-
-            var fakeSearchableEntityThree = new FakeSearchableEntityThree
-            {
-                SearchableStringValue = $"fakeSearchableEntityThree: {nameof(FakeSearchableEntityThree.SearchableStringValue)}",
-                CompressedStringValue = $"fakeSearchableEntityThree: {nameof(FakeSearchableEntityThree.CompressedStringValue)}",
-                RecursiveTypeReference = new FakeSearchableEntityThree
-                {
-                    SearchableStringValue = $"fakeSearchableEntityThree: {nameof(FakeSearchableEntityThree.RecursiveTypeReference)}.{nameof(FakeSearchableEntityThree.RecursiveTypeReference.SearchableStringValue)}",
-                    CompressedStringValue = $"fakeSearchableEntityThree: {nameof(FakeSearchableEntityThree.RecursiveTypeReference)}.{nameof(FakeSearchableEntityThree.RecursiveTypeReference.CompressedStringValue)}",
-                    RecursiveTypeReference = new FakeSearchableEntityThree
-                    {
-                        SearchableStringValue = $"fakeSearchableEntityThree: {nameof(FakeSearchableEntityThree.RecursiveTypeReference)}.{nameof(FakeSearchableEntityThree.RecursiveTypeReference.SearchableStringValue)}.{nameof(FakeSearchableEntityThree.RecursiveTypeReference.RecursiveTypeReference.SearchableStringValue)}",
-                        CompressedStringValue = $"fakeSearchableEntityThree: {nameof(FakeSearchableEntityThree.RecursiveTypeReference)}.{nameof(FakeSearchableEntityThree.RecursiveTypeReference.CompressedStringValue)}.{nameof(FakeSearchableEntityThree.RecursiveTypeReference.RecursiveTypeReference.CompressedStringValue)}"
-                    }
-                }
-            };
-
-            var entity = (dynamic)optimizer.ToEntityObject<FakeSearchableEntityThree>(fakeSearchableEntityThree);
-
-            Assert.NotNull(entity);
-
-            var searchableStringValue = entity.@string as string;
-            var compressedStringValue = entity.p as string;
-
-            // This may not work. Needs tested when bug is fixed.
-            var complexSelfSearchableStringValue = entity.complexself.@string as string;
-            var complexSelfCompressedStringValue = entity.complexself.p as string;
-
-            var complexSelfComplexSelfSearchableStringValue = entity.complexself.complexself.@string as string;
-            var complexSelfComplexSelfCompressedStringValue = entity.complexself.complexself.p as string;
-
-            Assert.Equal(fakeSearchableEntityThree.SearchableStringValue, searchableStringValue);
-            Assert.Equal("kdkwZmFrZVNlYXJjaGFibGVFbnRpdHlUaHJlZTogQ29tcHJlc3NlZFN0cmluZ1ZhbHVl", compressedStringValue);
-            Assert.Equal(fakeSearchableEntityThree.RecursiveTypeReference.SearchableStringValue, complexSelfSearchableStringValue);
-            Assert.Equal("kdlHZmFrZVNlYXJjaGFibGVFbnRpdHlUaHJlZTogUmVjdXJzaXZlVHlwZVJlZmVyZW5jZS5Db21wcmVzc2VkU3RyaW5nVmFsdWU=", complexSelfCompressedStringValue);
-            Assert.Equal(fakeSearchableEntityThree.RecursiveTypeReference.RecursiveTypeReference.SearchableStringValue, complexSelfComplexSelfSearchableStringValue);
-            Assert.Equal("kdldZmFrZVNlYXJjaGFibGVFbnRpdHlUaHJlZTogUmVjdXJzaXZlVHlwZVJlZmVyZW5jZS5Db21wcmVzc2VkU3RyaW5nVmFsdWUuQ29tcHJlc3NlZFN0cmluZ1ZhbHVl", complexSelfComplexSelfCompressedStringValue);
-        }
-
+        
         [Fact, TestPriorityOrder(104)]
         public void ToEntityObject_UnregisteredType()
         {
---
+++ dii.storage/TableTypeGenerator.cs
@@ -0,0 +1,170 @@
+﻿using dii.storage.Attributes;
+using dii.storage.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using static dii.storage.Optimizer;
+
+namespace dii.storage
+{
+    public class TableTypeGenerator : TypeGenerator
+    {
+        public TableTypeGenerator(Type source, ModuleBuilder builder, Dictionary<Type,Type> subPropertyMapping, bool suppressConfigurationErrors = false) : base(source, builder, subPropertyMapping, suppressConfigurationErrors)
+        { }
+
+        protected Dictionary<int, PropertyInfo> partitionFields { get; set; } = new Dictionary<int, PropertyInfo>();
+        protected Dictionary<int, PropertyInfo> idFields { get; set; } = new Dictionary<int, PropertyInfo>();
+
+        /// <summary>
+        /// The delimiter to separate aggregate partiton keys.
+        /// </summary>
+        protected char partitionSeparator { get; set; } = Constants.DefaultPartitionKeyDelimitor;
+
+        /// <summary>
+        /// The <see cref="Type"/> of the partition key.
+        /// </summary>
+        protected Type partitionKeyType { get; set; } = Constants.StringType;
+
+        /// <summary>
+        /// The delimiter to separate aggregate id keys.
+        /// </summary>
+        protected char idSeparator { get; set; } = Constants.DefaultIdDelimitor;
+
+        /// <summary>
+        /// Intentionally a no-op.  Table type mappings are intended to be handled within the optimizer.
+        /// </summary>
+        /// <param name="serializer"></param>
+        protected override void MapTypes(Serializer serializer)
+        {
+            //Do not call the base intentionally.  Table type mapping is handled by the caller.
+        }
+
+        /// <summary>
+        /// Processes the extended properties of a Table mapped type.
+        /// </summary>
+        /// <param name="p">The PropertyInfo from the source object being processed.</param>
+        protected override void ProcessExtended(PropertyInfo p)
+        {
+            base.ProcessExtended(p);
+            ProcessPartitionKey(p);
+            ProcessIdField(p);
+        }
+
+        /// <summary>
+        /// Processes any PartitionKey mappings and configuration
+        /// for the source property.
+        /// </summary>
+        /// <param name="p">The <see cref="PropertyInfo"/> of the source property.</param>
+        /// <returns>True if the property was a PartitionKey, false otherwise.</returns>
+        protected bool ProcessPartitionKey(PropertyInfo p)
+        {
+            var partitionKey = p.GetCustomAttribute<PartitionKeyAttribute>();
+            if (partitionKey != null)
+            {
+                //Two fields cannot occupy the same position in the partition key.
+                if (partitionFields.ContainsKey(partitionKey.Order))
+                {
+                    throw new DiiPartitionKeyDuplicateOrderException(partitionFields[partitionKey.Order].Name, p.Name, partitionKey.Order);
+                }
+
+                partitionFields.Add(partitionKey.Order, p);
+
+                //First PartitionKeyAttribute to change the separator wins.
+                //Example: If a the first occurring property sets the separator to / 
+                //and the second sets it to \ the first property that was encountered
+                //and set the separator will permanently set the separator for the type.
+                //So in the example, even though the separator sets twice, it will remain
+                // '/'
+                if (!char.IsWhiteSpace(partitionKey.Separator) && partitionSeparator == Constants.DefaultPartitionKeyDelimitor)
+                {
+                    partitionSeparator = partitionKey.Separator;
+                }
+
+                if (partitionKey.PartitionKeyType != null && partitionKey.PartitionKeyType != typeof(string))
+                {
+                    partitionKeyType = partitionKey.PartitionKeyType;
+                }
+
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Process the mapped IdAttribute to the table type
+        /// </summary>
+        /// <param name="p">The <seealso cref="PropertyInfo"/> being processed.</param>
+        /// <returns>True if the property was a IdField, false otherwise.</returns>
+        /// <exception cref="DiiIdDuplicateOrderException"></exception>
+        protected bool ProcessIdField(PropertyInfo p)
+        {
+            var id = p.GetCustomAttribute<IdAttribute>();
+            if (id != null)
+            {
+                if (idFields.ContainsKey(id.Order))
+                {
+                    // Throw an exception when an invalid type is attempted.
+                    throw new DiiIdDuplicateOrderException(idFields[id.Order].Name, p.Name, id.Order);
+                }
+
+                idFields.Add(id.Order, p);
+
+                // First IdAttribute to change the separator wins.
+                if (!char.IsWhiteSpace(id.Separator) && idSeparator == Constants.DefaultIdDelimitor)
+                {
+                    idSeparator = id.Separator;
+                }
+
+                return true;
+            }
+            return false;
+        }
+
+        protected override void AppendExtendedFields()
+        {
+            base.AppendExtendedFields();
+
+            //Append the necessary table fields for the PK and the ID
+            _ = AddProperty(typeBuilder, Constants.ReservedPartitionKeyKey, typeof(string));
+            _ = AddProperty(typeBuilder, Constants.ReservedIdKey, typeof(string));
+        }
+
+        protected override Serializer CustomizeSearchableSerializer(Dictionary<string, PropertyInfo> properties, Serializer serializer)
+        {
+            serializer = base.CustomizeSearchableSerializer(properties, serializer);
+
+            //Handle PK mapping for the stored type.
+            var partitionKeyInfo = properties[Constants.ReservedPartitionKeyKey];
+            serializer.PartitionKey = partitionKeyInfo;
+            properties.Remove(Constants.ReservedPartitionKeyKey);
+
+
+            var idInfo = properties[Constants.ReservedIdKey];
+            properties.Remove(Constants.ReservedIdKey);
+            serializer.Id = idInfo;
+
+            if (idFields != null && idFields.Any())
+            {
+                serializer.IdProperties = idFields
+                        .OrderBy(x => x.Key)
+                        .Select(x => x.Value)
+                        .ToList();
+                serializer.IdSeparator = idSeparator.ToString();
+            }
+
+            if (partitionFields != null && partitionFields.Any())
+            {
+                serializer.PartitionKeyProperties = partitionFields
+                        .OrderBy(x => x.Key)
+                        .Select(x => x.Value)
+                        .ToList();
+                serializer.PartitionKeySeparator = partitionSeparator.ToString();
+                serializer.PartitionKeyType = partitionKeyType;
+            }
+
+            return serializer;
+        }
+    }
+}
